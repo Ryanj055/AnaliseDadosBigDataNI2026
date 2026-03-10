85f81eb9a998465a20b10b802f7d58e3f79a3c0a
Ryanj055 <ryanj021@hotmail.com>
2026-03-09 21:27:03 -0300

    adicionar pasta

---
+++ aula02/aula02-04032026.py
@@ -0,0 +1,20 @@
+# pokebola1='bulbasaur' #string
+# pokebola2='psyduck'
+# pokebola3='clefairy'
+# idade=30    
+# nome='maria'
+# preco=19.99
+# print(type(preco))
+# vazio=Non
+
+##aula 01- variaveis
+##tipos de dados
+# decisao1=True
+# decisao2=False
+# print(vazio)
+# print(decisao1)
+# print(type(decisao1))
+# print(decisao2)
+# print(type(decisao2))
+
+##
---
+++ aula02/aula02.1.py
@@ -0,0 +1,32 @@
+# ##exercicio 01
+# nota1=7.5
+# nota2=8.5
+# media_notas= (nota1+nota2/2)
+# print(media_notas)
+
+# # exercicio 02
+
+
+# num1= 3
+# num2= 4
+
+# soma=num1+num2
+# subtracao=num1-num2
+# multiplicacao=num1*num2
+# divisao=num1/num2
+# modulo=num1%num2
+# print(soma,subtracao,multiplicacao,divisao,modulo)
+
+
+## partida de futebol
+placar_time1=1
+placar_time2=2
+resultado=0
+
+if placar_time1 > placar_time2: 
+    resultado= 'time 1 ganhou'
+elif placar_time1 < placar_time2:
+    resultado= 'time 2 ganhou'
+else:
+    resultado= 'empate'      
+
